0b0a591ae7e36856b6a249f2c51c33e3c1bbfdb2
Twenty7els <kagankov@bk.ru>
2025-06-17 12:27:40 +0300

    Add files via upload

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,9 +9,11 @@ export default function Hero({ onChooseBox }) {
         <div className="hero-company-name">BAD NEWS MEN'S</div>
         <div className="hero-slogan">Лаконичность. Сила. Контроль.</div>
         <p className="hero-description">Мужские уходовые боксы, собранные с характером.<br />Выбирай набор под свой стиль<br />Никакой мишуры — только польза, качество и уверенность</p>
-        <div className="hero-btn-group">
-          <button className="hero-btn" onClick={onChooseBox}>Выбрать набор</button>
-          <button className="hero-btn hero-btn-secondary">О компании</button>
+        <div className="hero-btn-container">
+          <div className="hero-btn-group">
+            <button className="hero-btn" onClick={onChooseBox}>Выбрать набор</button>
+            <button className="hero-btn hero-btn-secondary">О компании</button>
+          </div>
         </div>
       </div>
     </section>
